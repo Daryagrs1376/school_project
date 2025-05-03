b59559ebc1ce7b508e0c47993f8092c44dc333c4
Zahragrs <dryagaroosi@gmail.com>
2025-05-03 11:02:07 +0330

    Initial commit

--- student/admin.py
+++ student/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Student
 
 
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'age', 'is_active')
--- student/views.py
+++ student/views.py
@@ -4,7 +4,6 @@ from .models import Student
 from .serializers import StudentSerializer
 
 
-
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
