b4d1ea828296804e3db5304d0299d96234e157ec
Zahragrs <dryagaroosi@gmail.com>
2025-05-03 12:39:25 +0330

    Edit  crud modelviewset

--- student/serializers.py
+++ student/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Student
 
-
-
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'age', 'is_active'] 
+        fields = '__all__'
+
+    def validate_age(self, value):
+        if value < 5:
+            raise serializers.ValidationError("The age must be at least 5 years.")
+        return value
--- student/views.py
+++ student/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer
 
@@ -8,20 +7,11 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-
-    def create(self, request, *args, **kwargs):
-        age = int(request.data.get("age", 0))
-        if age < 5:
-            return Response({"error": "The age must be at least 5 years."}, status=status.HTTP_400_BAD_REQUEST)
-        return super().create(request, *args, **kwargs)
-
-
     def update(self, request, *args, **kwargs):
         student = self.get_object()
         if not student.is_active:
             return Response({"error": "You cannot edit an inactive student."}, status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
-
 
     def destroy(self, request, *args, **kwargs):
         student = self.get_object()
