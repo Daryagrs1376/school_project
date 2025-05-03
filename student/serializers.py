8cc9163ae3e217dcdde9620d04b513f4c81e080c
Zahragrs <dryagaroosi@gmail.com>
2025-05-03 13:57:38 +0330

    Add status student

--- student/serializers.py
+++ student/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 from .models import Student
 
+
+
 class StudentSerializer(serializers.ModelSerializer):
+    is_active_status = serializers.SerializerMethodField()
+
     class Meta:
         model = Student
         fields = '__all__'
+
+    
+    def get_is_active_status(self, obj):
+        if obj.is_active:
+            return "Active"
+        else:
+            return "Inactive"
+        
+
 
     def validate_age(self, value):
         if value < 5:
--- student/views.py
+++ student/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
+
 
 
 class StudentViewSet(viewsets.ModelViewSet):
@@ -13,8 +15,17 @@ class StudentViewSet(viewsets.ModelViewSet):
             return Response({"error": "You cannot edit an inactive student."}, status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
 
+
     def destroy(self, request, *args, **kwargs):
         student = self.get_object()
         student.is_active = False
         student.save()
         return Response({"message": "The student has been deactivated."}, status=status.HTTP_204_NO_CONTENT)
+
+
+    def check_status(self, request, *args, **kwargs):
+        student = self.get_object()
+        status = "Active" if student.is_active else "Inactive"
+        return Response({"status": status}, status=status.HTTP_200_OK)
+    
+    
