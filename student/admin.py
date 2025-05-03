51dd188d5c98490c636f6b01ab8781f634c90188
Zahragrs <dryagaroosi@gmail.com>
2025-05-03 10:54:21 +0330

    Initial commit

---
+++ student/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Student
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'age', 'is_active')
+    list_filter = ('is_active', 'age')
+    search_fields = ('first_name', 'last_name')
+    ordering = ('-id',)
---
+++ student/views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+    def create(self, request, *args, **kwargs):
+        age = int(request.data.get("age", 0))
+        if age < 5:
+            return Response({"error": "The age must be at least 5 years."}, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
+
+
+    def update(self, request, *args, **kwargs):
+        student = self.get_object()
+        if not student.is_active:
+            return Response({"error": "You cannot edit an inactive student."}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+
+    def destroy(self, request, *args, **kwargs):
+        student = self.get_object()
+        student.is_active = False
+        student.save()
+        return Response({"message": "The student has been deactivated."}, status=status.HTTP_204_NO_CONTENT)
